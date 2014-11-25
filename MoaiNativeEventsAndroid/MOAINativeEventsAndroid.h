4c08bc50cc9f2ed7655ee748e19f00e20294f82a
Jonah Wallerstein <jwallers@gmail.com>
2014-11-25 18:47:52 -0500

    Removed placeholders and added Android native event cpp files

---
+++ MoaiNativeEventsAndroid/MOAINativeEventsAndroid.h
@@ -0,0 +1,35 @@
+
+
+#ifndef	MOAINATIVEEVENTSANDROID_H
+#define	MOAINATIVEEVENTSANDROID_H
+
+#include <moai-core/headers.h>
+
+//================================================================//
+// MOAINativeEventsAndroid
+//================================================================//
+/**	@name	MOAINativeEventsAndroid
+	@text	Native event access for android devices.
+
+*/
+class MOAINativeEventsAndroid :
+	public MOAIGlobalClass < MOAINativeEventsAndroid, MOAILuaObject > {
+private:
+
+	MOAILuaStrongRef	mEventCallback;
+	
+        //----------------------------------------------------------------//
+        static int	_triggerEvent	( lua_State* L );
+        static int	_triggerCallbackEvent	( lua_State* L );
+
+public:
+	
+	DECL_LUA_SINGLETON ( MOAINativeEventsAndroid )
+
+			MOAINativeEventsAndroid		();
+			~MOAINativeEventsAndroid		();
+	void	NotifyEventFinished	( cc8* rslt );
+	void	RegisterLuaClass		( MOAILuaState& state );
+};
+
+#endif
--- MoaiNativeEventsAndroid/host.cpp
+++ MoaiNativeEventsAndroid/host.cpp
@@ -5,7 +5,7 @@
 #include <moai-core/headers.h>
 #include <moai-sim/headers.h>
 #include <moai-util/headers.h>
-#include "MOAIHello.h"
+#include "MOAINativeEventsAndroid.h"
 
 //----------------------------------------------------------------//
 void MOAINativeEventsAppFinalize () {
@@ -18,5 +18,5 @@ void MOAINativeEventsAppInitialize () {
 
 //----------------------------------------------------------------//
 void MOAINativeEventsContextInitialize () {
-	REGISTER_LUA_CLASS ( MOAIHello )
+	REGISTER_LUA_CLASS ( MOAINativeEventsAndroid )
 }
